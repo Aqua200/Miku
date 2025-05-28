c5ba3028711c9216d2d4e8fe161f501083d9105e
Neykoor💜 <chinquepapa@gmail.com>
2025-05-28 14:37:45 -0700

    Update anime-angry.js

--- plugins/anime-angry.js
+++ plugins/anime-angry.js
@@ -1,39 +1,35 @@
-/* 
-❀ Codígo creado por Destroy
-✧ https://github.com/The-King-Destroy/Yuki_Suou-Bot.git 
+/*
+❀ Código creado por Destroy
+✧ https://github.com/The-King-Destroy/Yuki_Suou-Bot.git
 */
 
-import fs from 'fs'
-import path from 'path'
+let handler = async (m, { conn }) => {
+  let who = m.mentionedJid.length > 0 
+    ? m.mentionedJid[0] 
+    : (m.quoted ? m.quoted.sender : m.sender)
 
-let handler = async (m, { conn, usedPrefix }) => {
-    let who = m.mentionedJid.length > 0 ? m.mentionedJid[0] : (m.quoted ? m.quoted.sender : m.sender)
-    let name = conn.getName(who)
-    let name2 = conn.getName(m.sender)
+  let name = conn.getName(who)
+  let name2 = conn.getName(m.sender)
 
-    let str = m.mentionedJid.length > 0 || m.quoted 
-        ? `\`${name2}\` está enojado/a \`${name || who}\` 凸ಠ益ಠ)凸` 
-        : `\`${name2}\` está enojado/a 凸ಠ益ಠ)凸`
-    
-    if (m.isGroup) {
-        let pp = 'https://files.catbox.moe/dnk7oc.mp4'
-        let pp2 = ''
-        let pp3 = ''
-        let pp4 = ''
-        let pp5 = ''
-        let pp6 = ''
-        let pp7 = ''
-        let pp8 = ''
-        let pp9 = ''
-        let pp10 = ''
-        let pp11 = ''
-        let pp12 = ''
-        
-        const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12]
-        const video = videos[Math.floor(Math.random() * videos.length)]
-        
-        conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption: str, ptt: true, mentions: [who] }, { quoted: m })
-    }
+  let str = m.mentionedJid.length > 0 || m.quoted 
+    ? `\`${name2}\` está enojado/a con \`${name || who}\` 凸ಠ益ಠ)凸` 
+    : `\`${name2}\` está enojado/a 凸ಠ益ಠ)凸`
+
+  if (m.isGroup) {
+    const videos = [
+      'https://files.catbox.moe/dnk7oc.mp4',
+      // Agrega más URLs si deseas variedad
+    ]
+
+    const video = videos[Math.floor(Math.random() * videos.length)]
+
+    await conn.sendMessage(m.chat, {
+      video: { url: video },
+      gifPlayback: true,
+      caption: str,
+      mentions: [who]
+    }, { quoted: m })
+  }
 }
 
 handler.help = ['angry']
