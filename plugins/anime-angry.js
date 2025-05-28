c626e21f15a2a85c59e62a37dd89206e697d0f47
Neykoor💜 <chinquepapa@gmail.com>
2025-05-28 14:31:15 -0700

    Update anime-angry.js

--- plugins/anime-angry.js
+++ plugins/anime-angry.js
@@ -16,18 +16,8 @@ let handler = async (m, { conn, usedPrefix }) => {
         : `\`${name2}\` está enojado/a 凸ಠ益ಠ)凸`
     
     if (m.isGroup) {
-        let pp = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742786883573.mp4'
-        let pp2 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742786889338.mp4'
-        let pp3 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742786895614.mp4'
-        let pp4 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742786900963.mp4'
-        let pp5 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742786913602.mp4'
-        let pp6 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742786877462.mp4'
-        let pp7 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742786829620.mp4'
-        let pp8 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742786871042.mp4'
-        let pp9 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742786865577.mp4'
-        let pp10 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742786855746.mp4'
-        let pp11 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742786847500.mp4'
-        let pp12 = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742786834604.mp4'
+        let pp = ''
+        
         
         const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12]
         const video = videos[Math.floor(Math.random() * videos.length)]
