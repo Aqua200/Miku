734088ae913d2095cd6fbc68756b0124d5dbb919
Neykoor💜 <chinquepapa@gmail.com>
2025-05-27 22:12:10 -0700

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -12,10 +12,10 @@ let handler = async (m, { conn, args }) => {
     let redes = global.redes || 'https://linktr.ee/tu_bot'; // Enlace a tus redes o un linktree (cambiado para ser un placeholder)
 
     let txt = `
-｡･ﾟﾟ･ sweetly greets 
-@${userId.split('@')[0]} ･ﾟﾟ･｡
-¡Holi! Soy
-*${botname}* 
+｡･ﾟﾟ･｡🩵･ﾟﾟ･｡
+Hola, @${userId.split('@')[0]} ✨  
+Soy *${botname}*, ¡un gustito tenerte aquí!  
+｡･ﾟﾟ･｡🩵･ﾟﾟ･｡
 
 ╭┈─────── ୨୧ ───────┈╮
 ┊  ❀  *Usuario:*  @${userId.split('@')[0]}
